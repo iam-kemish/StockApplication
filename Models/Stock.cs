23efcb6c66ab34d51dcbb6e6054f77dcf2acb571
kemish bajgai <kemishbajgai4@gmail.com>
2026-02-03 15:04:49 +0545

    Add project files.

---
+++ Models/Stock.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockApplication.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName =("decimal(18,2)"))]
+        public decimal Purchase {  get; set; }
+        [Column(TypeName =("decimal(18,2"))]
+
+        public decimal lastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; } 
+
+        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
+    }
+
+}

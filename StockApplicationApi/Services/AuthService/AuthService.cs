614e1418af8b2e3f2169b620cba520d44a4a91d2
Kemis <Kemis@DESKTOP-KM576VI>
2026-04-13 14:53:19 +0545

    feat:Add fluent validation for RegisterDTO

--- StockApplicationApi/Services/AuthService/AuthService.cs
+++ StockApplicationApi/Services/AuthService/AuthService.cs
@@ -1,9 +1,20 @@
-﻿using StockApplicationApi.Models.DTOs;
+﻿using Microsoft.AspNetCore.Identity;
+using StockApplicationApi.Models;
+using StockApplicationApi.Models.DTOs;
 
 namespace StockApplicationApi.Services.AuthService
 {
     public class AuthService : IAuthService
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<AppUser> _signinManager;
+        public AuthService(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signinManager = signInManager;
+        }
         public Task<AuthResponseDTO> Login(LoginDTO dto)
         {
             throw new NotImplementedException();
---
+++ StockApplicationApi/Validators/Auth/RegisterValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using StockApplicationApi.Models.DTOs;
+
+namespace StockApplicationApi.Validators.Auth
+{
+    public class RegisterValidator:AbstractValidator<RegisterDTO>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().MinimumLength(5);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+
+          
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters");
+        }
+    }
+}
